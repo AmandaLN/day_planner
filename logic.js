b0b8174e28a13cc8b6d3bdec41028c9ca7505aef
Amanda Nelson <amandaln4311@gmail.com>
2020-12-29 12:06:06 -0500

    created time blocks in JS instead of html

--- logic.js
+++ logic.js
@@ -1,22 +1,48 @@
-$(document).ready(function(){
-    console.log(moment().format('dddd, MMMM Do YYYY, h:mm:ss a')); // December 26th 2020, 11:11:40 am
-    
+$(document).ready(() => {
+    console.log(moment().format('dddd, MMMM Do YYYY, h:mm:ss a'));
+
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-    let currentHour = moment().hours();
+    // Global variables
+    let currentHour = parseInt(moment().hour());
     console.log(currentHour);
-
-    $(".time-block").each(function(){
-        for (let i = 0; i < 10; i++) {
-            
-            let blockHour = parseInt($(this).attr("id").split("-")[1]);
-
-            if ($(blockHour < currentHour).addClass(".past"));
-
-            else if ($(blockHour === currentHour).addClass(".present"));
-
-            else ($(blockHour > currentHour).addClass(".future"));
-        
+    let workHours = []
+    // Adding time blocks to HTML
+    // let timeOfDay = ["9", "10", "11", "13", "14", "15", "16", "17", "18"]
+    for (let hour = 9; hour < 18; hour++) {
+        workHours.push(moment({hour}).format('h  a'));
+        $('.container').append(`<div class='row time-block' <div id="hour">
+        <div class='col-md-1 hour'>
+          <p>${moment({hour}).format('h  a')}</p>
+        </div>
+            <textarea class="col-md-10 description"></textarea>
+                <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
+            </div>
+       </div>`);
     }
-    })
+
+    // Colorcoding the timeblocks
+    $("#time-block").each(function () {
+         let blockHour = parseInt($(this).attr("id"));
+
+        if (blockHour < currentHour) {
+             $(this).addClass("past");
+
+         } 
+         else if (blockHour === currentHour) {
+             $(this).addClass("present");
+
+         } 
+        else {
+             $(this).addClass("future");
+
+       }
+
+    });
+
+
+
+
+
 })
+
