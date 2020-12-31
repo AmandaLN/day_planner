d78426ff0b0f04dde6394cc5068ae34b1103f03c
Amanda Nelson <amandaln4311@gmail.com>
2020-12-30 21:25:39 -0500

    added the classes to time blocks

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Day Planner
 ## Description
-With this website you can add an event in a specific time frame. You will notice the blocks are color coded. Red is present time, green is future, and grey is past time. When refreshing the page, your event stays saved.
+With this app you can add an event in a specific time frame. You will notice the blocks are color coded. Red is present time, green is future, and grey is past time. When refreshing the page, your event stays saved.
 
 *https://amandaln.github.io/day_planner/
 
 ## Technologies
 HTML
+
 CSS/Bootstrap
+
 JavaScript/JQuery
 
 ## Usage
--- assets/logic.js
+++ assets/logic.js
@@ -6,41 +6,44 @@ $(document).ready(() => {
     // Global variables
     let currentHour = parseInt(moment().hour());
     console.log(currentHour);
-    let workHours = []
+    let workHours = [];
     // Adding time blocks to HTML
     // let timeOfDay = ["9", "10", "11", "13", "14", "15", "16", "17", "18"]
     for (let hour = 9; hour < 18; hour++) {
         workHours.push(moment({hour}).format('h  a'));
-        $('.container').append(`<div class='row time-block' <div id="hour">
-        <div class='col-md-1 hour'>
+        $('.container').append(`<div class="row time-block">
+        <div class="col-md-1 hour">
           <p>${moment({hour}).format('h  a')}</p>
         </div>
-            <textarea class="col-md-10 description"></textarea>
+            <textarea class="col-md-10 description" value = "${hour}"></textarea>
                 <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
             </div>
        </div>`);
     }
 
-    // Colorcoding the timeblocks
-    $("#time-block").each(function () {
-         let blockHour = parseInt($(this).attr("id"));
-
-        if (blockHour < currentHour) {
+    // Colorcoding the timeblocks by adding classes
+    $("textarea").each(function () {
+        
+         let blockHour = parseInt($(this).attr("value"));
+        
+         if (blockHour < currentHour) {
              $(this).addClass("past");
 
          } 
-         else if (blockHour === currentHour) {
+         if (blockHour === currentHour) {
              $(this).addClass("present");
 
          } 
-        else {
+         if (blockHour > currentHour) {
+            
              $(this).addClass("future");
 
        }
 
     });
 
-
+    // store to localstorage
+    
 
 
 
